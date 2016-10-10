dcb45208ef89f50eb82b7231a4decacf88b4822e
Josejulio Martínez <jmartine@redhat.com>
2016-10-10 10:36:30 -0500

    Updated to react to travis notifications.

--- hawkular_bot.rb
+++ hawkular_bot.rb
@@ -8,30 +8,43 @@ GITHUB_URL = 'https://api.github.com'.freeze
 TRAVIS_URL = 'https://api.travis-ci.org'.freeze
 ENABLED_REPOS = ['hawkular/hawkular-services'].freeze
 
-get '/job/:job' do
-  job_id = params['job']
-  github_token = ENV['GITHUB_TOKEN']
+post '/notifications' do
+  payload = request_body.fetch('payload', '')
+  signature = request.env['HTTP_SIGNATURE']
+  unless signature_is_valid signature, payload
+    status 401
+    return 'Invalid signature'
+  end
+  notification = payload.to_json
+  repo_slug = "#{notification['repository']['owner_name']}/#{notification['repository']['name']}"
+  unless ENABLED_REPOS.include? repo_slug
+    status 500
+    return "Unknown repository #{repo_slug}"
+  end
 
+  unless notification['type'] == 'pull_request'
+    status 500
+    return 'Only valid for pull-requests'
+  end
+  pull = notification['pull_request_number']
+
+  build_id = notification['id']
   travis_client = RestClient::Resource.new(TRAVIS_URL)
   travis_headers = {
     'accept'          => 'application/vnd.travis-ci.2+json',
     'accept-encoding' => 'gzip, deflate',
     'user-agent'      => 'hawkular-bot'
   }
-  job = JSON.parse(travis_client["jobs/#{job_id}"].get(travis_headers))
-  repo_slug = job['job']['repository_slug']
-
-  unless ENABLED_REPOS.include? repo_slug
-    status 500
-    return "Unknown repository #{repo_slug}"
+  build = JSON.parse(travis_client["builds/#{build_id}"].get(travis_headers))
+  build_status = HawkularBotUtils.status_unknown
+  build['builds']['job_ids'].each do |job_id|
+    job_log = travis_client["jobs/#{job_id}/log"].get 'accept' => 'text/plain', 'accept-encoding' => 'gzip, deflate'
+    job_status = HawkularBotUtils.ruby_test_status job_log
+    build_status = job_status unless job_status == HawkularBotUtils.status_unknown
+    break if build_status == HawkularBotUtils.status_failed
   end
 
-  pull = HawkularBotUtils.parse_pull_url job['commit']['compare_url']
-
-  build_log = travis_client["jobs/#{job_id}/log"].get 'accept' => 'text/plain', 'accept-encoding' => 'gzip, deflate'
-  ruby_status = HawkularBotUtils.ruby_test_status build_log
-
-  github_message = case ruby_status
+  github_message = case build_status
                    when :success
                      'This PR seem to be working well with '\
                      '[hawkular-client-ruby](https://github.com/hawkular/hawkular-client-ruby) :+1:.'
@@ -40,7 +53,7 @@ get '/job/:job' do
                      '[hawkular-client-ruby](https://github.com/hawkular/hawkular-client-ruby).'
                    else
                      status 500
-                     return "Did not detect any Ruby status on #{repo_slug}"
+                     return "Did not detect any Build status on #{repo_slug}"
                    end
 
   github_client = RestClient::Resource.new(GITHUB_URL)
@@ -51,4 +64,18 @@ get '/job/:job' do
     body: github_message
   }
   github_client["repos/#{repo_slug}/issues/#{pull}/comments"].post(github_comment_body.to_json, github_header)
+end
+
+def signature_is_valid(signature, payload)
+  pkey = OpenSSL::PKey::RSA.new(public_key)
+  pkey.verify(OpenSSL::Digest::SHA1.new, Base64.decode64(signature), payload)
+end
+
+def request_body
+  request.body.read
+end
+
+def public_key
+  client = RestClient::Resource.new(TRAVIS_URL)
+  client['config'].get['config']['notifications']['webhook']['public_key']
 end
--- lib/hawkular_bot_utils.rb
+++ lib/hawkular_bot_utils.rb
@@ -2,6 +2,22 @@ class HawkularBotUtils
   RUBY_SUCCEEDED_MESSAGE = '---Ruby tests succeeded---'.freeze
   RUBY_FAILED_MESSAGE = '---Ruby tests failed---'.freeze
 
+  STATUS_UNKNOWN = :unknown
+  STATUS_SUCCESS = :success
+  STATUS_FAILED = :failed
+
+  def self.status_unknown
+    STATUS_UNKNOWN
+  end
+
+  def self.status_success
+    STATUS_SUCCESS
+  end
+
+  def self.status_failed
+    STATUS_FAILED
+  end
+
   def self.parse_pull_url url
     pr = URI(url).path.split('/').last
     return pr if pr =~ /^\d+$/
@@ -10,11 +26,11 @@ class HawkularBotUtils
 
   def self.ruby_test_status build_log
     if build_log.include? RUBY_SUCCEEDED_MESSAGE
-      :success
+      STATUS_SUCCESS
     elsif build_log.include? RUBY_FAILED_MESSAGE
-      :failed
+      STATUS_FAILED
     else
-      :unknown
+      STATUS_UNKNOWN
     end
   end
 end
